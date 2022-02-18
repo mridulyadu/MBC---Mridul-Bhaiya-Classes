be8060c379873a3b3b64b633471e99b2908ca5b7
mridulyadu <69645403+mridulyadu@users.noreply.github.com>
2022-02-18 22:52:31 +0530

    MBC-Mridul Bhaiya Classes

--- JavaScript/MBC javascript.js
+++ JavaScript/MBC javascript.js
@@ -23,3 +23,27 @@ document.querySelectorAll('.course-3 .box .video video').forEach(vid =>{
 document.querySelector('#close-vid').onclick = () =>{
     mainVid.classList.remove('active');
 }
+
+var mobile = document.createElement('div');
+mobile.className = 'nav-mobile';
+document.querySelector('.nav').appendChild(mobile);
+
+// hasClass
+function hasClass(elem, className) {
+    return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+  }
+  
+  // toggleClass
+  function toggleClass(elem, className) {
+    var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, ' ') + ' ';
+      if (hasClass(elem, className)) {
+          while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
+              newClass = newClass.replace(' ' + className + ' ', ' ');
+          }
+          elem.className = newClass.replace(/^\s+|\s+$/g, '');
+      } else {
+          elem.className += ' ' + className;
+      }
+  }
+
+  
